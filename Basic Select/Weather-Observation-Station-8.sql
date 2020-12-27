da7aa7cf673d52d6330a8a435f61ead48fb7c967
Chirag Raote <chiragraote17@gmail.com>
2020-12-27 18:43:07 -0500

    Update Weather-Observation-Station-8.sql
    just my version of answer

--- Basic Select/Weather-Observation-Station-8.sql
+++ Basic Select/Weather-Observation-Station-8.sql
@@ -1,1 +1,22 @@
+#this works faster and better
 select distinct city from station where left(city,1) in('a','e','i','o','u') and right(city,1) in('a','e','i','o','u') 
+
+#easier to understand
+select distinct city from station where 
+(city like 'a%'
+or
+city like 'e%'
+or
+city like 'i%'
+or
+city like 'o%'
+or
+city like 'u%') and (city like '%a'
+or
+ city like '%e'
+or
+ city like '%i'
+or
+ city like '%o'
+or
+ city like '%u');
